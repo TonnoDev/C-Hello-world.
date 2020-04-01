2f0f2d841d3ef064d4fc2c9c0c89967ecdf61dd8
TonnoDev <63007094+TonnoDev@users.noreply.github.com>
2020-04-01 17:49:50 +0200

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+// start, inizio.
+	
+cout<<"Hello, World.";	
+	
+// print on screen, stampa su schermo.
+	
+return 0;
+}
+
+// end, fine.
